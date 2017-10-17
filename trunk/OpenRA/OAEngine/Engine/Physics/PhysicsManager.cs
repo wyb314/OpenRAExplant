e778a0f0903283059049a297c9234105a263abec
wyb314 <13212777425@163.com>
2017-10-17 20:41:11 +0800

    补充physics

---
+++ trunk/OpenRA/OAEngine/Engine/Physics/PhysicsManager.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Engine.Physics
+{
+    public class PhysicsManager
+    {
+        public enum PhysicsType { W_2D, W_3D };
+
+        public static IPhysicsManager instance;
+    }
+}
--- trunk/OpenRA/OAEngine/Engine/Physics/TSCollider2D.cs
+++ trunk/OpenRA/OAEngine/Engine/Physics/TSCollider2D.cs
@@ -5,6 +5,7 @@ using TrueSync.Physics2D;
 
 namespace Engine.Physics
 {
+    using World = TrueSync.Physics2D.World;
     public abstract class TSCollider2D : ICollider
     {
         private Shape shape;
@@ -48,12 +49,93 @@ namespace Engine.Physics
         }
 
         public TSMaterial tsMaterial;
-        
-        private TSVector2 center;
 
         private Vector3 scaledCenter;
 
+
+        protected TSVector lossyScale = TSVector.one;
+        
+        public TSVector2 ScaledCenter
+        {
+            get
+            {
+                return TSVector2.Scale(center, new TSVector2(lossyScale.x, lossyScale.y));
+            }
+        }
+
         internal Body _body;
+
+        public IBody2D Body
+        {
+            get
+            {
+                if (_body == null)
+                {
+                    CheckPhysics();
+                }
+
+                return _body;
+            }
+        }
+
+        private TSVector2 center;
+        public TSVector2 Center
+        {
+            get
+            {
+                return center;
+            }
+            set
+            {
+                center = value;
+            }
+        }
+
+        private void CheckPhysics()
+        {
+            if (_body == null && PhysicsManager.instance != null)
+            {
+                PhysicsManager.instance.AddBody(this);
+            }
+        }
+
+        public virtual Shape CreateShape()
+        {
+            return null;
+        }
+
+        public virtual Shape[] CreateShapes()
+        {
+            return new Shape[0];
+        }
+
+        private TSRigidBody2D tsRigidBody;
+        
+        public TSTransform2D tsTransform;
+
+        public TSRigidBody2D attachedRigidbody
+        {
+            get
+            {
+                return tsRigidBody;
+            }
+        }
+
+        public void Awake()
+        {
+
+            
+        }
+
+
+        public virtual void Tick()
+        {
+        }
+
+        public void Initialize(World world)
+        {
+            CreateBody(world);
+        }
 
 
     }
