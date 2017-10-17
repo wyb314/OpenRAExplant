246b332193ae066dcb32badde728a5fefd07fa84
wyb314 <13212777425@163.com>
2017-10-17 19:42:33 +0800

    物理部分补充

---
+++ trunk/OpenRA/OAEngine/Engine/Physics/Physics2DWorldManager.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using Physics;
+using TrueSync;
+using TrueSync.Physics2D;
+
+namespace Engine.Physics
+{
+    public class Physics2DWorldManager : IPhysicsManager
+    {
+        public static Physics2DWorldManager instance;
+
+        private World world;
+
+        Dictionary<IBody, IRandererObject> gameObjectMap;
+
+        //Dictionary<Body, Dictionary<Body, TSCollision2D>> collisionInfo;
+
+        public TSVector Gravity
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public FP LockedTimeStep
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public bool SpeculativeContacts
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public void AddBody(ICollider iCollider)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int GetBodyLayer(IBody rigidBody)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IRandererObject GetGameObject(IBody rigidBody)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IWorld GetWorld()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IWorldClone GetWorldClone()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Init()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsCollisionEnabled(IBody rigidBody1, IBody rigidBody2)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnRemoveBody(Action<IBody> OnRemoveBody)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveBody(IBody iBody)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateStep()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ trunk/OpenRA/OAEngine/Engine/Physics/TSCollider2D.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using TrueSync;
+using TrueSync.Physics2D;
+
+namespace Engine.Physics
+{
+    public abstract class TSCollider2D : ICollider
+    {
+        private Shape shape;
+       
+        public Shape Shape
+        {
+            get
+            {
+                if (shape == null)
+                    shape = CreateShape();
+                return shape;
+            }
+            protected set { shape = value; }
+        }
+
+        private bool _isTrigger;
+        
+        public bool isTrigger
+        {
+
+            get
+            {
+                if (_body != null)
+                {
+                    return _body.IsSensor;
+                }
+
+                return _isTrigger;
+            }
+
+            set
+            {
+                _isTrigger = value;
+
+                if (_body != null)
+                {
+                    _body.IsSensor = value;
+                }
+            }
+
+        }
+
+        public TSMaterial tsMaterial;
+        
+        private TSVector2 center;
+
+        private Vector3 scaledCenter;
+
+        internal Body _body;
+
+
+    }
+}
